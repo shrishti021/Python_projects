919a3dfb90e425389ae1453307e11de27b1a307a
dell@DESKTOP-G7LCHO7 <shrishtimaheshwari.sm707@gmail.com>
2026-03-22 00:31:31 +0530

    code change

--- ATM_pin_validator.py
+++ ATM_pin_validator.py
@@ -1,3 +1,9 @@
+# ATM PIN Validator
+# BEGINNER
+# 15 pts
+# 📌 Build an ATM-style PIN validation system. The user gets 3 attempts to enter the correct PIN.
+# 🎯 Task: Write a program using a while loop that: gives the user 3 attempts, locks the account after 3 wrong tries, and prints the remaining attempts each time.
+
 correct_pin = "1234"
 attempts_left = 3
 
